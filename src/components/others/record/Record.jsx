1c13bf73b35c5c2681317d44f58c09a758d08fbf
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2022-02-07 17:18:46 -0300

    security

--- src/components/others/record/Record.jsx
+++ src/components/others/record/Record.jsx
@@ -1,11 +1,19 @@
 import { StyledRecord } from "../../../style/record";
 
 export default function Record ({description, value, date, isPlus}) {
+    let src = "";
+    for(let i = 0; i<description.length; i++){
+        if(i===0){
+            src += description[i].toUpperCase();
+        } else {
+            src += description[i].toLowerCase();
+        }
+    }
     return(
         <StyledRecord isPlus={isPlus}>
             <p>
                 <strong>{date}</strong>
-                {description}
+                {src}
             </p>
             <span>{parseInt(value).toFixed(2).replace(".", ",")}</span>
         </StyledRecord>
