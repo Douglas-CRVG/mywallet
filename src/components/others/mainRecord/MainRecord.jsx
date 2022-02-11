e476cb1962c85ac4fdaed07a13c131a18840d0f0
Lucas Cardoso Tavares <carvares.dev@gmail.com>
2022-02-11 16:32:05 -0300

    preparando review

--- src/components/others/mainRecord/MainRecord.jsx
+++
@@ -1,8 +0,0 @@
-import { saveRecord } from "../../../utils/utils"
-import Form from "../form/Form"
-
-export default function MainRecord(){
-    return(
-        <Form inputs={ saveRecord} button="Salvar"/>
-    )
-}
